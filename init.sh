3b10b210922689afc526ed80e6558416cf9a63da
DmitriyYa <dmitriy.m.yakovlev@gmail>
2020-11-28 11:42:22 +0700

    fix init, default.conf

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx/conf.d/default.conf /etc/nginx/default.conf
+sudo ﻿ln -sf /home/box/web/etc/default.conf /etc/nginx/conf.d/default.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 ﻿﻿
