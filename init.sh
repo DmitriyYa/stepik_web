9fe9126e5ca2fe86377555cae1023e58de73e84c
DmitriyYa <dmitriy.m.yakovlev@gmail>
2020-11-28 11:07:02 +0700

    config ngix

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx/conf.d/default.conf /etc/nginx/default.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 ﻿﻿
