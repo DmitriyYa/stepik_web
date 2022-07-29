7fd5caa4d59571685d3b6a981e42da7c5a9fa17f
DmitriyYa <dmitriy.m.yakovlev@gmail>
2022-07-29 17:59:16 +0700

    fix gunicorn

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 def app(environ, start_response):
-        body = bytes('\n'.join('q=1&p=3&x=3&d=5&g=7'.split('&')), 'ascii')
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(body)))
-        ])
-        return iter([body])
+    body = '\n'.join(environ['QUERY_STRING'].split('&'))
+    body1 = bytes(body+'\n', 'ascii')
+    # body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(body1)))
+    ])
+    # print(body)
+    # print(body1)
+    return iter([body1])
--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo ﻿ln -sf /home/box/web/etc/default.conf /etc/nginx/conf.d/default.conf
+sudo ln -sf /home/box/web/etc/default.conf /etc/nginx/conf.d/default.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-﻿﻿
